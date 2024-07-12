2493e594c71571101f973a4b26f74e308caf6779
haripratapreddy <60550047+haripratapreddy@users.noreply.github.com>
2024-07-12 08:22:53 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "vpc_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "num_of_pub_subnets" {
+  type = number
+}
+
+variable "num_of_pri_subnets" {
+  type = number
+}
+
+variable "pub_subnet_cidr" {
+  type = list
+}
+
+variable "pri_subnet_cidr" {
+  type = list
+}
+
+variable "EIP" {
+  type = bool
+}
