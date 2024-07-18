e83143f0a4e1b05dc44aa46ba7edaa7cafce1435
haripratapreddy <60550047+haripratapreddy@users.noreply.github.com>
2024-07-18 05:50:33 +0000

    update backend.yaml and backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # bucket = var.bucket
-    # key    = var.bucket_key
+    key    = "terraform-state"
     # region = var.aws_region
     # dynamodb_table = var.dynamodb
   }
